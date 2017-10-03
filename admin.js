2c44396113bcf4d50dd63d2ca4b2404f6a94cab3
Zilong Wang <zilong0wang@gmail.com>
2017-10-03 23:48:48 +1100

    add admin router

---
+++ admin.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const _ = require('lodash')
+const uuid = require('node-uuid')
+var rooms = require('./data/rooms.json')
+
+const router = express.Router()
+module.exports = router
+
+router.get('/rooms', function (req, res) {
+    res.render('rooms', { title: 'Admin - Chatrooms', rooms: rooms })
+})
+
+router.route('/rooms/add')
+    .post(function (req, res) {
+        let room = {
+            name: req.body.name,
+            id: uuid.v4()
+        }
+        rooms.push(room)
+        res.redirect(`${req.baseUrl}/rooms`)
+    }).get(function (req, res) {
+        res.render('add', { title: 'Admin - Add Chatroom' })
+    })
+
+router.route('/rooms/edit/:id')
+    .all(function (req, res, next) {
+        let room = _.find(rooms, { id: req.params.id })
+        if (!room) {
+            res.sendStatus(404)
+            return
+        }
+        res.locals.room = room
+        next()
+    })
+    .get(function (req, res) {
+        res.render('edit', { title: 'Admin - Edit Chatroom', room: res.locals.room })
+    })
+    .post(function (req, res) {
+        res.locals.room.name = req.body.name
+        res.redirect(`${req.baseUrl}/rooms`)
+    })
+
+router.get('/rooms/delete/:id', function (req, res) {
+    rooms = rooms.filter(r => r.id !== req.params.id)
+    res.redirect(`${req.baseUrl}/rooms`)
+})
--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const uuid = require('node-uuid')
 const app = express()
 
 app.set('views', './views')
@@ -6,10 +8,14 @@ app.set('view engine', 'pug')
 
 app.use(express.static('./public'))
 app.use(express.static('./node_modules/bootstrap/dist'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', function (req, res) {
     res.render('index', { title: 'Chatroom Home' })
 })
+
+var adminRouter = require('./admin')
+app.use('/admin', adminRouter)
 
 app.listen(3000, function () {
     console.log('Chatroom app is listening on port 3000.')
