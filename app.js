aeb072226531870f394013f08a6185b609510080
Zilong Wang <zilong0wang@gmail.com>
2017-10-10 23:55:15 +1100

    add logging

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const app = express()
 app.set('views', './views')
 app.set('view engine', 'pug')
 
+var fs = require('fs')
+var path = require('path')
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+app.use(require('morgan')('combined', { stream: accessLogStream }))
+
 app.use(express.static('./public'))
 app.use(express.static('./node_modules/bootstrap/dist'))
 app.use(express.static('./node_modules/jquery/dist'))
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
